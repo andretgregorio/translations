dbde81f7b06fdb56861bcb0657ecdc41dcd21e7c
Andre Gregorio <andretgregorio@gmail.com>
2020-02-11 16:06:40 -0300

    fix: key and feature

--- classes/TranslationsOps.py
+++ classes/TranslationsOps.py
@@ -9,12 +9,13 @@ class TranslationsOps(object):
         translations = []
         for i, row in data_frame.iterrows():
             r = row.to_dict()
-            feature = r['Unnamed: 2']
-            key = r['Unnamed: 3']
-            values = row[4:].to_dict()
+            feature = r['Unnamed: 1']
+            key = r['Unnamed: 2']
+            values = row[3:].to_dict()
             for l, v in values.items():
                 locale = Locale(l)
                 value = Value(v, values[locale.fallback])
-                if locale.main in desired_locales:
-                    translations.append(Translation.factory(feature, key, value.get(), locale.get(), translation_status))
+                if locale.get() in desired_locales:
+                    translations.append(
+                        Translation.factory(feature, key, value.get(), locale.get(), translation_status))
         return translations
